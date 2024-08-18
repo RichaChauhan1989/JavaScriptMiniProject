342ee68de9d33c6f706b82564fa0baa9b90d75a3
RichaChauhan1989 <richasain1989@gmail.com>
2024-08-18 13:22:58 +1200

    Add progress step functionality

---
+++ create_stepper/script.js
@@ -0,0 +1,48 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const nxt = document.getElementById('nxt')
+const circles = document.querySelectorAll('.circle')
+
+let currentActive = 1
+
+nxt.addEventListener('click', () => {
+    currentActive++
+    if(currentActive > circles.length) {
+        currentActive = circles.length
+    }
+
+    update()
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+    if(currentActive < 1) {
+        currentActive = 1
+    }
+
+    update()
+})
+
+function update()
+{
+    circles.forEach((circle, idx) => {
+        if(idx < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+    })
+
+    const actives = document.querySelectorAll('.active');
+
+    progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circles.length) {
+        nxt.disabled = true
+    } else {
+        prev.disabled = false
+        nxt.disabled = false
+    }
+}
